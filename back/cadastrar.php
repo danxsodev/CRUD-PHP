a2d2010fd75cfb7eb996a7e49979ad86d7759804
Danillo Xavier <102252843+danxsodev@users.noreply.github.com>
2022-10-31 17:44:23 -0300

    Add files via upload

--- back/cadastrar.php
+++ back/cadastrar.php
@@ -1,12 +1,13 @@
 <?php
+    include
     include 'conexao.php';
 
     $nome = $_POST[txt-login];
     $senha = $_POST[txt-senha];
     $sexo = $_POST[radio-sexo];
 
-    $insert = $comando->query("insert into tbl_teste nm_usuario, senha_usuario, sexo_usuario
+    $insert = $comando->query("insert into tbl_teste nm_teste, senha_teste, sexo_teste
     values ('$nome', '$senha', '$sexo');");
 
-    echo "<script src='/front/src/script/cadastrar.js'></script>"
+    echo "<script src='/front/src/script/cadastrar.js'></script>";
 ?>
--- front/src/script/cadastrar.js
+++ front/src/script/cadastrar.js
@@ -1,1 +1,10 @@
-window.alert("Cadastrados realizado com sucesso!");
+const btnSubmit = document.getElementById("btn-submit");
+btnSubmit.addEventListener("click", function(){
+    window.alert("Cadastro realizado com sucesso!");
+})
+
+
+const btnBackMenu = document.getElementById("btn-back");
+btnBackMenu.addEventListener("click", function(){
+    window.history.back();
+})
