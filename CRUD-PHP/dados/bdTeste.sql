c9f4de971542277fa64a099c2367e58b5fcfb43d
Danillo Xavier <102252843+danxsodev@users.noreply.github.com>
2022-11-03 17:21:51 -0300

    Add files via upload

---
+++ CRUD-PHP/dados/bdTeste.sql
@@ -0,0 +1,21 @@
+create database bdTeste;
+
+use bdTeste;
+
+create table tbUsers (
+id int primary key auto_increment,
+username varchar(20) not null,
+senha varchar(10) not null,
+sexo char(2) not null);
+
+insert into tbUsers
+values (
+	default,
+	'Robson', 
+	'1234', 
+	'M'
+);
+
+desc tbUsers;
+
+select * from tbUsers;
