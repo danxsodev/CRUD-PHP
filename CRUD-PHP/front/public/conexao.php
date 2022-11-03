1d9f30cd24d6852fe112677d8b6bf372f9454796
Danillo Xavier <102252843+danxsodev@users.noreply.github.com>
2022-11-03 17:21:18 -0300

    Delete CRUD-PHP directory

--- CRUD-PHP/front/public/conexao.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $servidor = "localhost";
-    $usuario = "root";
-    $senha = "12345678";
-    $banco = "bdTeste";
-    $comando = new PDO("mysql:host=$servidor; dbname=$banco", $usuario, $senha);
-?>
