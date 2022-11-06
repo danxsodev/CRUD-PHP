ae5d70dbeabf9134551c80eecf07412739c95ebb
Danillo Xavier <102252843+danxsodev@users.noreply.github.com>
2022-11-06 18:51:53 -0300

    Delete back directory

--- back/conexao.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $servidor = "localhost";
-    $usuario = "root";
-    $senha = "12345678";
-    $banco = "bd_teste";
-    $comando = new PDO("mysql:host=$servidor; dbname=$banco", $usuario, $senha);
-
-?>
